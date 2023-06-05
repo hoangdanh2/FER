c2dc88711299acf7d550a2ca8d783a40d746f5c5
DANH <danhthse173598@fpt.edu.vn>
2023-06-05 14:20:38 +0700

    Components Part 1

---
+++ .history/src/App_20230605135815.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Navigation from './components/Navigation';
+function App() {
+  return (
+    <div className='App'>
+      <Navigation/>
+    </div>
+  );
+}
+export default App;
---
+++ .history/src/components/Player_20230605140301.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+export default class player extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="column">
+                    <div className="card">
+                        <img src="assets/images/cr.jpg" alt=""/>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
